4a64bbed4c6ac321e06cba49611dc6b75fc18e01
Fpalma11 <fabriciopalma17@gmail.com>
2022-07-12 19:01:10 -0600

    commit 12 julio 40%

---
+++ src/main/java/Proyecto_TurismoCLONE/Proyecto_Turismo/ProyectoTurismoCloneApplication.java
@@ -0,0 +1,13 @@
+package Proyecto_TurismoCLONE.Proyecto_Turismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTurismoCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTurismoCloneApplication.class, args);
+	}
+
+}
