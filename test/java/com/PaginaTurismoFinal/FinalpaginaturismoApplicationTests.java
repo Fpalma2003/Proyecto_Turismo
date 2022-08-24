df5387081248a98ef42c4851e91c54e9b5e7e86a
andraaraya <105759445+andraaraya@users.noreply.github.com>
2022-08-23 18:06:22 -0600

    Add files via upload

---
+++ test/java/com/PaginaTurismoFinal/FinalpaginaturismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PaginaTurismoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalpaginaturismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
